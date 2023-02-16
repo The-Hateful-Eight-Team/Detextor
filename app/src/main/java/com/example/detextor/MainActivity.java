f0844b463de7db78c868510d7f1cc36f88d1384a
Egor Chitorkin <egor.chitorckin@yandex.ru>
2023-02-16 23:11:39 +0400

    add camera opening

--- app/src/main/java/com/example/detextor/MainActivity.java
+++ app/src/main/java/com/example/detextor/MainActivity.java
@@ -1,14 +1,164 @@
 package com.example.detextor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = "logs";
+    private CameraManager mCameraManager = null;
+    CameraService[] myCameras = null;
+    private TextureView mImageView = null;
+    private final int CAMERA1 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        //получение разрешений на доступ к камере и файловой системе
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                ||
+                (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        )
+        {
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try{
+            // Получение списка камер с устройства
+            myCameras = new CameraService[mCameraManager.getCameraIdList().length];
+            for (String cameraID : mCameraManager.getCameraIdList()) {
+                Log.i(LOG_TAG, "cameraID: "+cameraID);
+                int id = Integer.parseInt(cameraID);
+                // создаем обработчик для камеры
+                myCameras[id] = new CameraService(mCameraManager,cameraID);
+            }
+        }
+        catch(CameraAccessException e){
+            Log.e(LOG_TAG, e.getMessage());
+            e.printStackTrace();
+        }
+        mImageView = findViewById(R.id.textureView);
+        mImageView.setOnClickListener(v -> {
+            if (myCameras[CAMERA1] != null) {
+                if (!myCameras[CAMERA1].isOpen()) myCameras[CAMERA1].openCamera();
+            }
+        });
+    }
+
+    public class CameraService {
+        private final String mCameraID;
+        private CameraDevice mCameraDevice = null;
+        private CameraCaptureSession mCaptureSession;
+
+        public CameraService(CameraManager cameraManager, String cameraID) {
+
+            mCameraManager = cameraManager;
+            mCameraID = cameraID;
+        }
+
+        public boolean isOpen() {
+
+            return mCameraDevice != null;
+        }
+
+
+        public void openCamera() {
+            try {
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+                    mCameraManager.openCamera(mCameraID,mCameraCallback,null);
+                }
+            }
+            catch (CameraAccessException e) {
+                Log.i(LOG_TAG,e.getMessage());
+            }
+        }
+
+
+        public void closeCamera() {
+
+            if (mCameraDevice != null) {
+                mCameraDevice.close();
+                mCameraDevice = null;
+            }
+        }
+
+        private void createCameraPreviewSession() {
+
+            SurfaceTexture texture = mImageView.getSurfaceTexture();
+            Surface surface = new Surface(texture);
+
+            try {
+                final CaptureRequest.Builder builder =
+                        mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+
+                builder.addTarget(surface);
+                mCameraDevice.createCaptureSession(Collections.singletonList(surface),
+                        new CameraCaptureSession.StateCallback() {
+
+                            @Override
+                            public void onConfigured(CameraCaptureSession session) {
+                                mCaptureSession = session;
+                                try {
+                                    mCaptureSession.setRepeatingRequest(builder.build(),null,null);
+                                } catch (CameraAccessException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            @Override
+                            public void onConfigureFailed(CameraCaptureSession session) { }}, null );
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private CameraDevice.StateCallback mCameraCallback = new CameraDevice.StateCallback() {
+
+            @Override
+            public void onOpened(CameraDevice camera) {
+                mCameraDevice = camera;
+                Log.i(LOG_TAG, "Open camera  with id:"+mCameraDevice.getId());
+
+                createCameraPreviewSession();
+            }
+
+            @Override
+            public void onDisconnected(CameraDevice camera) {
+                mCameraDevice.close();
+
+                Log.i(LOG_TAG, "disconnect camera  with id:"+mCameraDevice.getId());
+                mCameraDevice = null;
+            }
+
+            @Override
+            public void onError(CameraDevice camera, int error) {
+                Log.i(LOG_TAG, "error! camera id:"+camera.getId()+" error:"+error);
+            }
+        };
     }
 }
